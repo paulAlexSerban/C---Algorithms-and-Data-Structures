acc7a9ac1a9e3f358c13cada54ba62cfee66dec6
paulAlexSerban <paul.alex.serban@gmail.com>
2022-01-23 14:44:38 +0200

    example / enumsExample

---
+++ workbook/05__enumsExample/source/enumsExample.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  enum Company {GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT};
+
+  enum Company xerox = XEROX;
+  enum Company facebook = FACEBOOK;
+  enum Company google = GOOGLE;
+  enum Company ebay = EBAY;
+
+  printf("The value of xerox is %d\n", xerox);
+  printf("The value of facebook is %d\n", facebook);
+  printf("The value of google is %d\n", google);
+  printf("The value of ebay is %d\n", ebay);
+
+  return 0;
+}
