eb90c9bab718905d6c8d82120ac5889665577ad3
paulAlexSerban <paul.alex.serban@gmail.com>
2022-01-24 15:10:26 +0200

    workbook

---
+++ workbook/09__printByteSizeOfBasicDataTypes/source/printByteSizeOfBasicDataTypes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  printf("Variabel of type char occupy %u\n", sizeof(char));
+  printf("Variabel of type short occupy %u\n", sizeof(short));
+  printf("Variabel of type int occupy %u\n", sizeof(int));
+  printf("Variabel of type long occupy %u\n", sizeof(long));
+  printf("Variabel of type long long occupy %u\n", sizeof(long long));
+  printf("Variabel of type float occupy %u\n", sizeof(float));
+  printf("Variabel of type double occupy %u\n", sizeof(double));
+  printf("Variabel of type double long occupy %u\n", sizeof(double long));
+}
